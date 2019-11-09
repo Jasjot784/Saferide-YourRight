fb5d168e3da9d7e18ca5e7cdd8fa0359f169d305
Jasjot Singh <jasjot784@gmail.com>
2019-11-09 16:52:10 +0530

    logo(icon) commit

---
+++ app/src/main/java/com/jasjotsingh/saferide_yourright/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jasjotsingh.saferide_yourright;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
